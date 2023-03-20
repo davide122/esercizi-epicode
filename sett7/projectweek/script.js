8731b179bc323e2be8f29aba03271112ceb5a3f6
davide122 <davidemarchica277@gmail.com>
2023-03-20 01:13:27 +0100

    commiy

--- sett7/projectweek/script.js
+++ sett7/projectweek/script.js
@@ -32,21 +32,22 @@ Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDE0MjQy
         const newdiv = document.createElement("div")
         newdiv.classList.add("col")
         newdiv.innerHTML =`
-        <div class="row me-5 my-5 justify-content-center align-items-center">
+        
         <div class="col col-12 col-md-12 col-lg-6 col-xl-6 col-xxl-6">
-        <div class="card h-100">
-        <img src="${ev.imageUrl}" class="card-img-top" alt="..." height="400px">
+        <div class="card " style="width: 300px;">
+        <img src="${ev.imageUrl}" class="card-img-top" alt="..."  >
         <div class="text-center amazonBlu">
           <h5 class="amazonBlu my-3">${ev.name}</h5>
           <p class="card-text amazonBlu">${ev.description}</p>
+          <p class="card-text amazonBlu">${ev.price}$</p>
    <div class="d-flex mx-1 justify-content-center">
           <a href ="dettagli.html?descrizione=${ev._id}" class=" d-flex btn btn-warning my-1 justify-content-center align-items-center">dettagli</a>
           <a href ="backoffice.html?ripopola=${ev._id}" class=" d-flex btn btn-danger my-1 mx-2 justify-content-center align-items-center">Modifica</a>
 
 </div>
           </div>
-        <div class="card-footer text-center " style="">
-        <small class="amazonBlu" >last update:${ev.updatedAt.slice(0,10)+" " + ev.updatedAt.slice(11,19)}</small>
+        <div class="card-footer text-center amazonCard" style="">
+        <small class="text-light" >last update:${ev.updatedAt.slice(0,10)+" " + ev.updatedAt.slice(11,19)}</small>
         </div>
   
     
